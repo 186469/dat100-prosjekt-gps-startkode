d210dffa5bc21039cb5f1c013f3f1de6314e4a59
Krus-hvl <186463@stud.hvl.no>
2024-10-15 13:28:09 +0200

    oppgave 1 ferdig

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// TODO
+		GPSPoint point = new GPSPoint(1, 2.0, 3.0, 5.0);
+		
+		System.out.println("Initial time: " + point.getTime());
+		
+		point.setTime(2);
+		
+		System.out.println("Updated GPSPoint: " + point.toString());
 		
 	}
 
